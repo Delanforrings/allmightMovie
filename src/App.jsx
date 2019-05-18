3f679a9809e35230db2a190bc82ff29649d1b455
zshi2 <zshi2@student.unimelb.edu.au>
2019-05-19 09:23:56 +1000

    build deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route} from 'react-router-dom'
 import {Animation} from 'mdbreact';
 import Home from "./pages/Home";
 import Landing from "./pages/Landing";
